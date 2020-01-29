3c3cb5e59ab4846e8e8f5fe9afb5eb769d9a5efb
Liam Keery <l_keery@fanshaweonline.ca>
2020-01-29 10:15:14 -0500

    Basic functionality complete

--- public/js/main_vm.js
+++ public/js/main_vm.js
@@ -1,15 +1,52 @@
 // imports always go first - if we're importing anything
+import ChatMessage from "./modules/ChatMessage.js";
 
 const socket = io();
 
-function setUserId(packet) {
+function setUserId({sID}) {
     //debugger;
-    console.log(packet);
+    console.log(sID);
+    vm.socketID = sID;
 }
 
 function showDisconnectMessage() {
     console.log('A user disconnected');
 }
 
+function appendMessage(message) {
+    vm.messages.push(message);
+}
+
+const vm = new Vue({
+    data: {
+        socketID: "",
+        message: "",
+        nickname: "",
+        messages: []
+    },
+
+    methods: {
+        dispatchMessage() {
+            console.log('Handle emit message');
+
+            socket.emit('chat_message', { 
+                content: this.message,
+                name: this.nickname || "anonymous"
+            })
+
+            this.message = "";
+        }
+    },
+
+    mounted: function() {
+        console.log('Vue done mounting');
+    },
+
+    components: {
+        newmessage: ChatMessage
+    }
+}).$mount("#app");
+
 socket.addEventListener('connected', setUserId);
 socket.addEventListener('disconnect', showDisconnectMessage);
+socket.addEventListener('new_message', appendMessage);
